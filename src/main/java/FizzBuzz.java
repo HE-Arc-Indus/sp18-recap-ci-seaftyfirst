0ea064e99a1484ea497aef7c82eede236d82f324
odilee <odile.eichenberger@he-arc.ch>
2018-05-14 19:19:57 +0200

    test

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class FizzBuzz {
+  public List<String> genererListe(int longueur) {
+    List<String> list = new ArrayList<String>();
+    for (int i = 1; i <= longueur; i++) {
+
+      list.add(String.valueOf(i));
+
+    }
+    return list;
+  }
 
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,5 +1,23 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class FizzBuzzTest {
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
 
+class FizzBuzzTest {
+  FizzBuzz fizzBuzz = new FizzBuzz();
+
+  @Test
+  public void testGenererList() {
+    //ARrange
+    fizzBuzz.genererListe(3);
+    List<String> expected = Arrays.asList("1", "2", "3");
+    //Act
+    List<String> method = fizzBuzz.genererListe(3);
+    //ASsert
+    assertThat(method.size(), is(method.size()));
+
+  }
 }
