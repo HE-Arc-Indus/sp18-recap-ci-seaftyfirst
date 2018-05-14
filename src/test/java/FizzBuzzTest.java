8427bb094fcfcc5e0ac4eb1b8587bb7dd47e9cb8
odilee <odile.eichenberger@he-arc.ch>
2018-05-14 11:49:02 +0200

    methode generaateList

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,5 +1,13 @@
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class FizzBuzzTest {
+
 
 }
